ae92ded348a993631bd607f7b90f0359771165e5
Aegon387 <aegon23@icloud.com>
2026-01-23 15:34:27 +0200

    fix: correct import paths in player.tsx

--- components/player/player.tsx
+++ components/player/player.tsx
@@ -1,7 +1,7 @@
 ﻿"use client"
 
-import { MiniPlayer } from "./mini-player"
-import { FullPlayer } from "./full-player"
+import { MiniPlayer } from "../mini-player"
+import { FullPlayer } from "../full-player"
 
 export function Player() {
   return (
